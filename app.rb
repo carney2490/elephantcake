03c7b1120395f1dc99bf35fdf234f974b370f6ce
JTong <eldertong@gmail.com>
2017-01-09 13:29:04 -0500

    beef jerky to clarify

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ number_of_items = ""
 
     puts "How many of the first item would you like to purchase?"
     number_of_items= gets.chomp
-    puts "Okay you would like to purchase #{number_of_items} items."
+    puts "Okay you would like to purchase #{number_of_items} items.  How much is the price of each item?"
+    price_of_each_item= gets.chomp
+    puts "Excellent, you want #{number_of_items} items at #{price_of_each_item} each!"
+    
 
 
