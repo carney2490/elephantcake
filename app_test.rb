908feab51fa912bab8a96c7133a768dc32d232dc
JTong <eldertong@gmail.com>
2017-01-09 13:11:10 -0500

    begining a test

---
+++ app_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "app.rb"
+
+class TestCoinSorter <Minitest::Test
+	def input_number_of_items_returns_number_of_items
+		number_of_items = 25
+		assert_equal(25, number_of_items(number))
+	end
+	
+end
