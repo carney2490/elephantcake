4f61aec873c87af290285c71259526ef1aa3d7cd
JTong <eldertong@gmail.com>
2017-01-09 13:14:45 -0500

    we remembered

--- app_test.rb
+++ app_test.rb
@@ -3,7 +3,7 @@ require_relative "app.rb"
 
 class TestElephantCake <Minitest::Test
 	def test_input_number_of_items_returns_number_of_items
-		number_of_items = 25
+		number = 25
 		assert_equal(25, number_of_items(number))
 	end
 	
