8ef269edf343f8c5d7f5f1a13274a26a48636c5a
JTong <eldertong@gmail.com>
2017-01-09 13:11:37 -0500

    change name

--- app_test.rb
+++ app_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require_relative "app.rb"
 
-class TestCoinSorter <Minitest::Test
+class TestElephantCake <Minitest::Test
 	def input_number_of_items_returns_number_of_items
 		number_of_items = 25
 		assert_equal(25, number_of_items(number))
