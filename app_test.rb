d2ce22e9d156a19e2a38b015343d937f74668835
JTong <eldertong@gmail.com>
2017-01-09 13:16:35 -0500

    test failed

--- app_test.rb
+++ app_test.rb
@@ -2,9 +2,14 @@ require "minitest/autorun"
 require_relative "app.rb"
 
 class TestElephantCake <Minitest::Test
-	def test_input_number_of_items_returns_number_of_items
+	def test_input_25_returns_25
 		number = 25
 		assert_equal(25, number_of_items(number))
+	end
+
+    def test_input_2_returns_2
+		number = 2
+		assert_equal(2, number_of_items(number))
 	end
 	
 end
