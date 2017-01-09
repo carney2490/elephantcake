82c42654c57d01de763460ff580f7c004cbe26b5
JTong <eldertong@gmail.com>
2017-01-09 13:14:14 -0500

    we forgot how to code?

---
+++ app.rb
@@ -0,0 +1,5 @@
+number_of_items = ""
+
+def number_of_items(number)
+    number = 25
+end
--- app_test.rb
+++ app_test.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require_relative "app.rb"
 
 class TestElephantCake <Minitest::Test
-	def input_number_of_items_returns_number_of_items
+	def test_input_number_of_items_returns_number_of_items
 		number_of_items = 25
 		assert_equal(25, number_of_items(number))
 	end
